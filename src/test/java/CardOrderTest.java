3bb8434d069d0a3ca4c5c916a7aa667e707c8f40
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-04-02 16:59:59 +0300

    add test commit

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.className("input__control")).sendKeys("Иванов Иван");
+        driver.findElement(By.className("input__control")).sendKeys("+79055936545");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals (expected, actual);
+    }
+
+
+}
+
