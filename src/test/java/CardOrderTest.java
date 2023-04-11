76a6444fb5e373cc1dec557974621f8015ead9a0
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-04-11 20:45:35 +0300

    edit BeforeEach commit

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -19,10 +19,7 @@ class ChromeTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
 
